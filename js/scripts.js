42e9205c973f48d59d8e6f1673cbab3430062368
Carl Sustrich <carl.sustrich@gmail.com>
2022-12-08 12:32:22 -0800

    create repository and necessary files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/* 
+order object
+  cost:
+  pizzas: array of pizza objects
+
+
+pizza object
+  size:
+  basic toppings array (.length * basic surcharge)
+  expensive toppings array (.length * expensive surcharge)
+  cost: 
+
+  
