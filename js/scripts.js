504b195fcd2c8e557c1739f25e2da0f18700ec45
Carl Sustrich <carl.sustrich@gmail.com>
2022-12-08 23:07:05 -0800

    finish UI integration to display all ordered pizza details

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function Order () {
   this.cost = 0;
   this.pizzas = [];
 }
+
 let newOrder = new Order();
 
 function Pizza () {
@@ -18,6 +19,11 @@ Pizza.prototype.calculateTotal = function () {
   this.size === "large" ? this.cost += 12 : this.size === "medium" ? this.cost += 10 : this.size === "small" ? this.cost += 8 : this.cost += 0;
   this.cost += ((this.basicToppings.length) * 2);
   this.cost += ((this.premiumToppings.length) *4);
+};
+
+Pizza.prototype.orderToString = function () {
+  let pizzaString = `${this.size} Pizza <br> Basic Toppings: <br> ${this.basicToppings} <br> Premium Toppings: <br> ${this.premiumToppings} <br> Cost: \$${this.cost} <br><br>`;
+  return pizzaString;
 }
 
 Order.prototype.calculateTotal = function () {
@@ -26,9 +32,9 @@ Order.prototype.calculateTotal = function () {
   newOrder.pizzas.forEach (function(pizza) {
     totalCost += pizza.cost;
     return totalCost;
-  })
+  });
   this.cost += totalCost;
-}
+};
 
 //UI
 
@@ -37,7 +43,7 @@ function pullCheckedValuesFor(name) {
   let arr = [];
   checkedboxes.forEach(function (checkbox) {
     if (checkbox.checked) {
-      arr.push(checkbox.value)
+      arr.push(checkbox.value);
     }
   });
   return arr;
@@ -61,13 +67,62 @@ function updateUI() {
   document.querySelector("#totalCost").innerText = newOrder.cost;
 }
 
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+// .innerHtml instead of innertext??
+
+function displayPizzaInfo() {
+  let pizzaDetailsHolder = document.querySelector('#individualPizzaDetails');
+  pizzaDetailsHolder.innerHTML = null;
+  newOrder.pizzas.forEach(function(pizza){
+    pizzaDetailsHolder.innerHTML += pizza.orderToString();
+  });
+}
+
+// function displayPizzaInfo() {
+//   let pizzaDetailsHolder = document.querySelector('#individualPizzaDetails');
+//   let pizzaP = document.createElement("p");
+
+//   newOrder.pizzas.forEach(function (pizza) {
+//     let pizzaUL = document.createElement('ul');
+
+//     let sizeLI = document.createElement('li');
+//     sizeLI.innerHTML = `Size: ${pizza.size} <br>`;
+//     let bTLI = document.createElement('li');
+//     bTLI.innerHTML = `Basic Toppings: ${pizza.basicToppings.join(", ")}<br>`;
+//     let pTLI = document.createElement('li');
+//     pTLI.innerHTML = `Premium Toppings: ${pizza.premiumToppings.join()}<br>`;
+//     let costLI = document.createElement('li');
+//     costLI.innerHTMl = `Cost: ${pizza.cost} <br>`;
+
+//     pizzaUL.append(sizeLI);
+//     pizzaUL.append(bTLI);
+//     pizzaUL.append(pTLI);
+//     pizzaUL.append(costLI);
+//     pizzaP.append(pizzaUL);
+//   });
+//   console.log(pizzaP)
+//   pizzaDetailsHolder.innerHTML = pizzaP;
+// }
+
 function handleFormSubmission (event) {
   event.preventDefault();
   makePizza();
   document.getElementById("new-pizza").reset();
   updateUI();
+  displayPizzaInfo();
 }
-
 
 window.addEventListener("load", function (){
   document.querySelector("form#new-pizza").addEventListener("submit", handleFormSubmission);
